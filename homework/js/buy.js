f04da1339efb6e31addb48c5a00a96d67f6abf1f
KimJeongHyo1 <wjdgy1129@naver.com>
2023-11-21 13:58:29 +0900

    실습문제

--- homework/js/buy.js
+++ homework/js/buy.js
@@ -1,11 +1,24 @@
 function printValue() {
-    const purchaseFrm = document.getElementsByName("purchaseFrm");
-    console.log(purchaseFrm, typeof purchaseFrm);
-    purchaseFrm.innerHTML = "구입자 : "+ purchaseFrm
+    const inputs = document.getElementsByTagName("input"); // 복수개
+    const userName = inputs[0].value;
+    const productName = document.getElementById("pname").value;
+    const prices = document.getElementsByName("price"); // 복수개
+    const price = prices[0].value;
 
+    const area = document.getElementById("area");
+    area.innerHTML = "구입자 : " + userName + "님<br>";
+    area.innerHTML += "구입상품 : " + productName + "<br>";
+    area.innerHTML += "구매가격 : " + price + "만원"
+
+    // 초기화
+    // 1. input태그별 
+    // for(let i = 0; i < inputs.length; i++)
+    // 	inputs[i].value = '';
+
+    // 2. 폼단위
+    // document.querySelector("[name=purchaseFrm]");
+    // document.forms[0]
+    // document.forms.purchaseFrm
+    document.purchaseFrm.reset(); // 폼하위의 input태그 value속성 초기화
+    
 }
-
-// function pname(){
-//     const pname = document.getElementsByTagName();
-//     console.log(pname[0].value);
-// }
